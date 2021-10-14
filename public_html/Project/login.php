b296edd5de54c2c016ae9c745bd31c7a9342dff5
Neil Patel <np656@njit.edu>
2021-10-14 14:57:08 -0400

    Register, login, profile, 002_alter_table_users_username

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -61,7 +61,7 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
     } else {
         //TODO 4
         $db = getDB();
-        $stmt = $db->prepare("SELECT email, password from Users where email = :email");
+        $stmt = $db->prepare("SELECT id, username, email, password from Users where email = :email");
         try {
             $r = $stmt->execute([":email" => $email]);
             if ($r) {
